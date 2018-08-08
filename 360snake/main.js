604ab73277030d00bc376cfd0c77cf72bb833e41
yoavsarfaty <yoav.sarfaty@gmail.com>
2018-08-08 19:42:20 +0300

    more better

--- 360snake/main.js
+++ 360snake/main.js
@@ -8,6 +8,8 @@ let scl;
 let snakeLength;
 let foodtime;
 let restarttime;
+let record = 0;
+let camAngle;
 
 
 function setup() {
@@ -41,13 +43,11 @@ function draw() {
     snakeLength += 10;
     createFood();
     for (let i = 0; i < 10; i++) {
-      // snake.addSegment(2 * scl);
+      // add to end of snake
       let old = snake;
       snake = new Segment(2 * scl, old.posI(0).x, old.posI(0).y);
       snake.child = old;
     }
-    // pos.add(p5.Vector.mult(vel, 10 * scl));
-    // snake.follow(pos.x, pos.y);
   }
   if (keyCode == RIGHT_ARROW && keyIsPressed) {
     angle += 0.1;
@@ -59,19 +59,28 @@ function draw() {
   vel.y = sin(angle) * scl;
 
   translate(width / 2, height / 2);
-  stroke(0, 0, 0, 100);
+  stroke(155);
+  strokeCap(PROJECT);
   noFill();
   strokeWeight(4 * scl);
   push();
-  rotate(atan2(pos.x, pos.y));
+  let target = atan2(pos.x, pos.y);
+  let delta = target - camAngle;
+  if (delta > PI) camAngle += TWO_PI;
+  else if (delta < -PI) camAngle -= TWO_PI;
+  delta = lerp(camAngle, target, .03) - camAngle;
+  delta = constrain(delta, -.1, .1);
+  camAngle += delta;
+  rotate(camAngle);
 
   ellipse(0, 0, radius * 2, radius * 2);
   for (let a = -PI; a < PI - 0.01; a += TWO_PI / 6) {
-    let dx = cos(a) * (radius - (4 * scl)),
-      dy = sin(a) * (radius - (4 * scl));
+    let dx = cos(a) * (radius),
+      dy = sin(a) * (radius);
     line(0, 0, dx, dy);
   }
   stroke(0);
+  strokeCap(ROUND);
   snake.show();
   strokeWeight(8 * scl);
   point(pos.x, pos.y);
@@ -82,15 +91,26 @@ function draw() {
   point(food.x, food.y);
 
   pop();
+  noFill();
+  strokeWeight(5);
+  for (let i = -radius * 1.1; i <= 0; i++) {
+    let inter = map(i, -radius, 0, 0, 1);
+    let c = lerpColor(color(255, 100), color(255, 0), inter);
+    stroke(c);
+    line(-radius, i, radius, i);
+  }
   fill(0);
   noStroke();
-  textSize(15 * scl);
-  let mytext = "360 SNAKE \n Created by Yoav Sarfaty \n Points: " + ((snakeLength * 10) - 100);
+  textSize(12 * scl);
+  let score = (snakeLength * 10) - 100;
+  record = max(score, record);
+  let mytext = "360 SNAKE\nCreated by Yoav Sarfaty\nPoints: " + score + "\nRecord: " + record;
   textAlign(CENTER);
-  text(mytext, 0, -radius / 2);
+  text(mytext, 0, -2 * radius / 3);
 }
 
 function restart() {
+  camAngle = HALF_PI;
   snakeLength = 10;
   snake = new Segment(2 * scl, 0, 0);
   pos = createVector(0, 0);
